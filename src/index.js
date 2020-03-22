5869b42df7947652ad645a5aa4ba453b9248e4ad
qlixxxy <sqlixor@gmail.com>
2020-03-22 19:22:47 +0300

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = []
+  if (matrix == undefined) {
+    return arr
+  }
+  for (let i of matrix) {
+      if (matrix.indexOf(i) % 2 == "0" || matrix.indexOf(i) == "0") {
+          arr.push(...i)
+      } else {
+        arr.push(...i.sort((a,b) => b - a))
+      }
+  } return arr
+
 }
